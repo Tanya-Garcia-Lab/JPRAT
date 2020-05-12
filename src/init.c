7ee25df075d19fce3120a584345880c8655e181d
Tanya Garcia <37775239+tpgarcia@users.noreply.github.com>
2020-05-12 17:10:52 -0500

    Update init.c file with correct number of arguments in kmjack

--- src/init.c
+++ src/init.c
@@ -2,15 +2,6 @@
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
-/*
-The following name(s) appear with different usages
-e.g., with different numbers of arguments:
-
-kmjack
-
-This needs to be resolved in the tables and any declarations.
-*/
-
 /* FIXME:
 Check these declarations against the C/Fortran source code.
 */
@@ -19,13 +10,11 @@ Check these declarations against the C/Fortran source code.
 extern void F77_NAME(get_count_new)(void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(get_count_outside)(void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(kmjack)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
-extern void F77_NAME(kmjack)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 
 static const R_FortranMethodDef FortranEntries[] = {
   {"get_count_new",     (DL_FUNC) &F77_NAME(get_count_new),      7},
   {"get_count_outside", (DL_FUNC) &F77_NAME(get_count_outside),  7},
   {"kmjack",            (DL_FUNC) &F77_NAME(kmjack),            15},
-  {"kmjack",            (DL_FUNC) &F77_NAME(kmjack),            16},
   {NULL, NULL, 0}
 };
 
