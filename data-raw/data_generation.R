dc06f81cf93dad5208a5feb823e6bb67d0b984ac
Unkyung Lee <uklee1029@gmail.com>
2020-07-18 09:40:34 -0500

    erase index column for data sets

--- data-raw/data_generation.R
+++ data-raw/data_generation.R
@@ -1,13 +1,13 @@
 ## predict
 data_predict<-read.csv("data-raw/data_predict.csv")
-
+data_predict<-data_predict[,-1]
 
 #save(data_predict, file="data/data_predict.rdata")
 usethis::use_data(data_predict, overwrite = TRUE)
 
 ## cohort
 data_cohort<-read.csv("data-raw/data_cohort.csv")
-
+data_cohort<-data_cohort[,-1]
 
 #save(data_cohort, file="data/data_cohort.rdata")
 usethis::use_data(data_cohort, overwrite = TRUE)
@@ -15,7 +15,7 @@ usethis::use_data(data_cohort, overwrite = TRUE)
 
 ## pharos
 data_pharos<-read.csv("data-raw/data_pharos.csv")
-
+data_pharos<-data_pharos[,-1]
 
 #save(data_pharos, file="data/data_pharos.rdata")
 usethis::use_data(data_pharos, overwrite = TRUE)
